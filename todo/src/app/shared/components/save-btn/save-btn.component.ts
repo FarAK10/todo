f1f4c407952a0cffcee912401c6af4350361bc2d
farak10 <botirovfarhod115@gmail.com>
2024-05-31 16:23:33 +0500

    feat: add base structure

---
+++ todo/src/app/shared/components/save-btn/save-btn.component.ts
@@ -0,0 +1,29 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  input,
+  OnInit,
+  output,
+} from '@angular/core';
+import { SubmitButtonStyle } from '../../typings/button';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-save-btn',
+  templateUrl: './save-btn.component.html',
+  styleUrls: ['./save-btn.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [CommonModule],
+})
+export class SaveBtnComponent {
+  isDisabled = input<boolean>(false);
+  isError = input<boolean>(false);
+  onClick = output<void>();
+
+  styles = computed<SubmitButtonStyle>(() => ({
+    error: this.isError(),
+    blocked: this.isDisabled(),
+  }));
+}
