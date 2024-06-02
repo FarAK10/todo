53bd25eb645cb221078a06bdcab85dc76774501a
Farkhod Botirov <00017037@wiut.uz>
2024-06-02 21:49:03 +0500

    refactor: use createurl tree in authguard

--- todo/src/app/core/guards/auth.guard.ts
+++ todo/src/app/core/guards/auth.guard.ts
@@ -2,17 +2,13 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { RootRoutes } from '../constants/routes';
-import { Store } from '@ngrx/store';
-import { take, tap } from 'rxjs';
+import { map } from 'rxjs';
+
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  return authService.isAuthenticated().pipe(
-    tap((isAuth)=>{
-       if(!isAuth){
-         router.navigate([RootRoutes.auth])
-       }
-    }),
 
-  )
+  return authService
+    .isAuthenticated()
+    .pipe(map((isAuth) => isAuth || router.createUrlTree([RootRoutes.auth])));
 };
