88ac76389d9e4b33f888a9c3aa44e89c9e3bfbbf
Farkhod Botirov <00017037@wiut.uz>
2024-06-02 21:48:29 +0500

    fix: back btn

--- todo/src/app/shared/components/back-btn/back-btn.component.ts
+++ todo/src/app/shared/components/back-btn/back-btn.component.ts
@@ -2,19 +2,16 @@ import { Location } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-back-btn',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, RouterModule],
   templateUrl: './back-btn.component.html',
   styleUrl: './back-btn.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BackBtnComponent {
   private location = inject(Location);
-
-  goBack() {
-    this.location.back();
-  }
 }
